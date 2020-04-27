70a859e0b0cf58f00d634163bda9348764dfb3c4
Siwa Runwat <sirocu@hotmail.com>
2020-04-28 00:57:51 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -5,10 +5,12 @@
 require "vendor/autoload.php";
 
 $access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+//$access_token = 'j3TpPDRdPt+bPQkcBxGmzaRFmvzVhSbwalp0oGqQGTxv3NYeq2ASalGT6K02DO2mmTFHqmFzBb2Tmv+qPm+MUJ6IpJykQdtFtmo/A/XGaSFM8R4vL+sppCs7er2S/iWGN/J7BHpwRoPZIueQ/sqBrQdB04t89/1O/w1cDnyilFU=';
 
-$channelSecret = '75c03f392f6e53d662d6f5a8db9e421f';
+//$channelSecret = '75c03f392f6e53d662d6f5a8db9e421f';
+$channelSecret = '058227fd278642d31374ec2b3a0a1c55';
 
-$pushID = 'U7ef7a449f2a5c2057eacfc02ba2eb286';
+$pushID = 'Uec36e7ef10fefc1b2a26f329628a80ed'; //Me
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
