358dcef93bac4eeff9d4686f57b9e18d11ea4843
Siwa Runwat <sirocu@hotmail.com>
2020-04-28 01:25:11 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -5,12 +5,13 @@
 require "vendor/autoload.php";
 
 $access_token = 'Lj66bFMe6xCC+2vg7OzflrfMtzIG8ETXHeuhWswdsJU7Aw+SM7nirKYCwWnAUfqrL+JSb+SB1ZO7GgsUmHhrefAtoVzzPZhuJvlngyGwzds7Fjy7aO4q5Oa5/BjCeShNXXd+SOsJhf+U5xP5Eic4TgdB04t89/1O/w1cDnyilFU=';
-$channelSecret = '058227fd278642d31374ec2b3a0a1c55';
+//$channelSecret = '058227fd278642d31374ec2b3a0a1c55';
 //$pushID = 'Uec36e7ef10fefc1b2a26f329628a80ed';
 //$pushID = 'U0742065967c6d99240b1df71d1cdbc3a';
 
 //$access_token = 'j3TpPDRdPt+bPQkcBxGmzaRFmvzVhSbwalp0oGqQGTxv3NYeq2ASalGT6K02DO2mmTFHqmFzBb2Tmv+qPm+MUJ6IpJykQdtFtmo/A/XGaSFM8R4vL+sppCs7er2S/iWGN/J7BHpwRoPZIueQ/sqBrQdB04t89/1O/w1cDnyilFU=';
 //$channelSecret = '29a3b0b7a2c0ede78a5c169e230206ba';
+$channelSecret = 'b2cbbed989cb65220871ef14c6b11d76';
 $pushID = 'Uac069b56e27c635574a678d77c0f1f4b'; //Minie
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
