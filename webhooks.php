2a4857ea7521290952d468277e8ee1d62dc943c5
Siwa Runwat <sirocu@hotmail.com>
2020-04-27 22:53:32 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -23,8 +23,8 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text     //Remove by SiRoCu 2018-06-08 (Remove UID response to sender)
-				//'text' => ''
+				//'text' => $text     //Remove by SiRoCu 2018-06-08 (Remove UID response to sender)
+				'text' => ''
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
