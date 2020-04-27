7432ee6f13ec13f03f236479a22afd16d9245b40
Siwa Runwat <sirocu@hotmail.com>
2020-04-28 00:41:22 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -8,14 +8,14 @@ $access_token = 'Lj66bFMe6xCC+2vg7OzflrfMtzIG8ETXHeuhWswdsJU7Aw+SM7nirKYCwWnAUfq
 $channelSecret = '058227fd278642d31374ec2b3a0a1c55';
 $pushID = 'Uec36e7ef10fefc1b2a26f329628a80ed';
 
-$access_token = "j3TpPDRdPt+bPQkcBxGmzaRFmvzVhSbwalp0oGqQGTxv3NYeq2ASalGT6K02DO2mmTFHqmFzBb2Tmv+qPm+MUJ6IpJykQdtFtmo/A/XGaSFM8R4vL+sppCs7er2S/iWGN/J7BHpwRoPZIueQ/sqBrQdB04t89/1O/w1cDnyilFU=";
+$access_token = 'j3TpPDRdPt+bPQkcBxGmzaRFmvzVhSbwalp0oGqQGTxv3NYeq2ASalGT6K02DO2mmTFHqmFzBb2Tmv+qPm+MUJ6IpJykQdtFtmo/A/XGaSFM8R4vL+sppCs7er2S/iWGN/J7BHpwRoPZIueQ/sqBrQdB04t89/1O/w1cDnyilFU=';
 $channelSecret = '29a3b0b7a2c0ede78a5c169e230206ba';
 $pushID = 'Uac069b56e27c635574a678d77c0f1f4b';
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 $MySMS = $_GET['sms'];
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('มีคำสั่งซื้อใหม่เข้ามา');
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('There is new orders coming in.');
 $response = $bot->pushMessage($pushID, $textMessageBuilder);
 
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
